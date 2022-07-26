d59f01fa85fb16f541cf37185fa906dae71d2951
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-26 18:04:45 -0400

    Update

--- Everything/Searching.cs
+++ Everything/Searching.cs
@@ -2,5 +2,9 @@ namespace Everything;
 
 public static class Searching
 {
-    public static IEnumerable<string> Search(string path, string query) => Map.Mapping(path).Where(x=>x.Contains(query));    
+    public static IEnumerable<string> Search(string path, string query) => 
+        Map.Mapping(path).
+        Select(x=>(x,x.ToLower())).
+        Where(x=>x.Item2.Contains(query.ToLower())).
+        Select(x=>x.Item1);    
 }
