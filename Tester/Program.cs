e595d7aa6be982d07922cda2db8232a12b7c5dd3
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-29 13:49:28 -0400

    Update

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,10 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var x = Searching.Search(@"C:\Users\vekt0R\Documents\Personal chest","Arcane");
-        foreach (var item in x)
-        {
-            System.Console.WriteLine(item);
-        }
+        var x = Searching.Search(@"C:\Users\vekt0R\Documents\Personal chest", "barbaren");
+        x.ForAll(System.Console.WriteLine);
+    }
+}
+internal static class Utils
+{
+    public static void ForAll<T>(this IEnumerable<T> e, Action<T> s)
+    {
+        foreach (var item in e) s(item);
     }
 }
