c53e132fb6c6ddb20e79e11b27238f9dd0d4af4c
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-30 20:01:35 -0400

    Refactoring

--- Everything/Map.cs
+++ Everything/Map.cs
@@ -2,26 +2,5 @@ namespace Everything;
 
 public static class Map
 {
-    static string[] BlackList = {  };
-
-    public static IEnumerable<string> Mapping(string root)
-    {
-        yield return root;
-
-        var directories = Directory.GetDirectories(root).Where(x => !BlackList.Contains(x));
-        var files = Directory.GetFiles(root);
-
-
-        foreach (var item in files)
-        {
-            yield return item;
-        }
-        foreach (var item in directories)
-        {
-            foreach (var item2 in Mapping(item))
-            {
-                yield return item2;
-            }
-        }
-    }
+    
 }
--- Everything/Searching.cs
+++ Everything/Searching.cs
@@ -2,9 +2,32 @@ namespace Everything;
 
 public static class Searching
 {
+    static string[] BlackList = {  };
     public static IEnumerable<string> Search(string path, string query) => 
-        Map.Mapping(path).
+        Mapping(path).
         Select(x=>(x,x.ToLower())).
         Where(x=>x.Item2.Contains(query.ToLower())).
-        Select(x=>x.Item1);    
+        Select(x=>x.Item1);
+
+    public static IEnumerable<string> Mapping(string root)
+    {
+        yield return root;
+
+        var directories = Directory.GetDirectories(root).Where(x => !BlackList.Contains(x));
+        var files = Directory.GetFiles(root);
+
+
+        foreach (var item in files)
+        {
+            yield return item;
+        }
+
+        foreach (var item in directories.AsParallel())
+        {
+            foreach (var item2 in Mapping(item))
+            {
+                yield return item2;
+            }
+        }
+    }
 }
